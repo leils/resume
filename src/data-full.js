1d23bf170582251ed3aeb3735609bdb307e0f5ab
Leia <e.leiachang@gmail.com>
2022-04-25 23:20:26 -0400

    add extended

---
+++ src/data-full.js
@@ -0,0 +1,111 @@
+const data = {
+  name: "Leia Chang",
+  email: "e.leiachang@gmail.com",
+  phone: "562-338-6171",
+  website: "www.leiac.me",
+  tagline: "Creative technologist making charming interactions",
+  sections: [
+    {
+    name: "Masters",
+    items: [{
+      summary1: "New York University",
+      summary2: "Masters at the Interactive Telecommunications Program",
+      dates: "Graduating May 2023",
+      bullets: [
+        "Creating immersive storytelling experiences embodied in the physical world",
+        "Exploring site-specific installation and spatial position tracking",
+        "Investigating non-traditional interfaces for intuitive interaction and expression"
+      ],
+    }, {
+      summary1: "Fabrication Workshop Staff",
+      dates: "Fall 2021",
+      bullets: [
+        "Assisting students in woodshop, laser cutting, 3d printing",
+        "Provides guidance in rapid fabrication and prototyping",
+      ],
+    }]
+  },{
+    name: "Experience",
+    items: [{
+      summary1: "Square Inc",
+      summary2: "Senior Software Engineer",
+      summary3: "Improving internal support tools",
+      dates: "Sept 2020 – May 2021",
+      bullets: [
+        "Project lead for internal tooling feature releases, guiding teams of 2-3 engineers",
+        "Communication with customers, from user research to stakeholder management",
+        "Collaboration and consultation with stakeholders during platform transitions"
+      ],
+    }, {
+      summary1: "Square Inc",
+      summary2: "Software Engineer",
+      summary3: "Product frontend engineering",
+      dates: "July 2017 – Sept 2020",
+      bullets: [
+        "Full-stack development in Ruby and Javascript (Ember) with a focus on Frontend",
+        "Lead Engineer on feature-release projects guiding teams of 2-3 engineers",
+        "Shadowed in project management & design for internal features"
+      ],
+    }, {
+      summary1: "Previous Internships",
+      dates: "2013 - 2016",
+      bullets: [
+        "Square Inc, Capital Consumer Org",
+        "Google Inc, Maps",
+        "EMC Dell, QA"
+      ]
+    }, {
+      summary1: "California Polytechnic San Luis Obispo",
+      summary2: "B.S. in Computer Science",
+      dates: "Graduated June 2017",
+      bullets: [
+        "Senior Project - Collaborated with dancers to create movement and sound-responsive costuming for use in performance"
+      ]
+    }]
+  }, {
+    name: "Misc",
+    items: [{
+      summary1: "Gray Area Immersive",
+      summary2: "3 month immersive interactive art program",
+      dates: "Fall 2019",
+      bullets: [
+        "Created Unclear, an interactive video display exploring the complexities and contradictions inherent to meditation",
+        "Work shown at Fall 2019 Gray Area Artist Showcase",
+      ],
+    }, {
+      summary1: "Square Women in Engineering",
+      summary2: "Diversity Group Lead",
+      dates: "2018 - 2020",
+      bullets: [
+        "Organized events encouraging community building and mentorship",
+        "Promoted changes in hiring and promotion practices",
+        "Trained and mentored incoming software engineers"
+      ],
+    }, {
+      summary1: "Blues and Tango dancing - Cal Poly SLO and SF",
+      summary2: "Teacher and Community Organizer",
+      dates: "2014 - 2020",
+      bullets: [
+        "Organized schedules for teachers, workshops, field trips",
+        "Taught weekly 2-hour lessons for beginner and intermediate dancers",
+        "Trained incoming teachers and generated lesson plans"
+      ]
+    }]
+  }, {
+    name: "Skills",
+    items: [{
+      text: [
+        "Fluent in frontend frameworks (Ember, React) and software engineering best practices",
+        "Proficient in microcontroller hardware including Arduino",
+        "Familiar with mechanical fabrication; laser cutting, 3D printing, introductory Solidworks and Fusion 360",
+        "Experience with working in Adobe Suite softwares (Illustrator, Photoshop, Premiere, Audition)",
+        "Comfortable with media documentation including audio/visual editing and storytelling",
+        "Experienced teacher and mentor",
+        "Quick to pick up new technologies and concepts"
+      ],
+    }]
+  }]
+}
+
+
+export default data
--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import { PDFViewer } from '@react-pdf/renderer';
 import './index.css';
 import Resume from './Resume';
 
-import data from './data'
+import data from './data';
+import dataExtended from './data-full';
+
+console.log(window.location.href);
+
+const pagePath =
+  typeof window !== "undefined" ? window.location.pathname.split("/")[1] : ""
+
+const desiredData = ((pagePath === "extended") ? dataExtended : data);
 
 ReactDOM.render(
   <PDFViewer width="100%" height="100%">
-    <Resume {...data} />
+    <Resume {...desiredData} />
   </PDFViewer>,
   document.getElementById('root')
 );
